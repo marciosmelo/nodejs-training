5824980dd8bc1481af2513acde470c56540c9dea
marciomelo@gmail.com <marciomelo@gmail.com>
2018-01-14 18:14:20 -0300

    Creating my firsts modules in node

---
+++ w3cTraining/demo_http.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+//create a server object
+http.createServer(function (req, res){
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.write('Hello World!');
+	res.end(); //end the response
+}).listen(8080); //the server object listens on this port
--- w3cTraining/helloWorld.js
+++ w3cTraining/helloWorld.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var dt = require('./myFirstModule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hello World! The Date and Time are currently: ' + dt.myDateTime());
 }).listen(8080);
